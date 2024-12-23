66ef374655662ea9371930ff2e8b35edec2e9e85
root <root@xzm.>
2024-12-23 12:56:56 +0000

    chore: docstrings for prettytable

--- prettytable.py
+++ prettytable.py
@@ -1,18 +1,22 @@
 class PrettyTable:
-    def __init__(self, headers, data):
+    def __init__(self, headers: list, data: list[list])-> None:
         """
         Initializes the PrettyTable with headers and data.
 
-        :param headers: List of column headers.
-        :param data: List of rows, where each row is a list of column values.
+        Args:
+            headers (list): List of headers for the table
+            data (list[list]): 2d array of the data to be presented
         """
         self.headers = headers
         self.data = data
         self.column_widths = [max(len(str(item)) for item in col) for col in zip(*data, headers)]
         
-    def get_table(self):
+    def get_table(self)-> str:
         """
         Prints the table to the console with nice formatting
+        
+        Returns:
+            string: returns the table in string format
         """
         table_str = ""
         header_row = " | ".join(f"{header:^{self.column_widths[i]}}" for i, header in enumerate(self.headers))        
